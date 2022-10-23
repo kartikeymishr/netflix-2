f1afe64d82701f32e46f4f1795d6429a955f1baa
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-10-23 21:05:54 +0530

    Fix for unknown routes.

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 </Routes>
                 :
                 <Routes>
-                    <Route path="/" element={<HomeScreen/>}/>
+                    <Route path="/*" element={<HomeScreen/>}/>
                     <Route path="profile" element={<ProfileScreen/>}/>
                 </Routes>
             }
